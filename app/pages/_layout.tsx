f7705603e0cf42c829129d1afd5212a8306e44ec
Alex-coder-dev <abdexter@utas.edu.au>
2026-03-15 04:38:22 +1100

    Add files via upload
    Mostly Firebase code that does a basic login. Users can sign up, verily their email and log in. After logging in nothing happens due to profile setup screen not being done yet.

---
+++ app/pages/_layout.tsx
@@ -0,0 +1,19 @@
+import { Slot } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
+import { useAuth } from "../src/hooks/useAuth";
+
+export default function RootLayout() {
+  const { initializing } = useAuth();
+
+  if (initializing) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Slot />;
+}
+
+
---
+++ app/pages/useAuth.ts
@@ -0,0 +1,19 @@
+import type { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { onAuthStateChanged } from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+
+export function useAuth() {
+  const [user,         setUser]         = useState<FirebaseAuthTypes.User | null>(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authenticatedUser) => {
+      setUser(authenticatedUser);
+      setInitializing(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  return { user, initializing };
+}
