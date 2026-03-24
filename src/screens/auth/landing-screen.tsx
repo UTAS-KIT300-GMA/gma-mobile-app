d2d8b383cf80ef5374536f8dd03a06a62fb218d2
Ngoc Tuan (Steve) Cao <68628829+cntuan90@users.noreply.github.com>
2026-03-24 23:09:21 +1100

    Update styles, navigation (#8)
    * struc: updated wih new header options, for main screens and subscreens.
    
    1. Main screens: Have Title, Notifications and Profile buttons
    2. Subscreens: Have back button and Tittle
    
    * style, struc: change style to match with Figma prototype and brand colouring. Change guest link to forgot password redirection link.
    
    ---------
    
    Co-authored-by: Vanni Pham <vannhi.pham@utas.edu.au>

--- src/screens/auth/landing-screen.tsx
+++ src/screens/auth/landing-screen.tsx
@@ -1,5 +1,17 @@
+import { colors } from "@/theme/ThemeProvider";
 import { Link, useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+/* This file defines the `LandingScreen` component, which serves as the initial screen of the app. 
+
+- It features the app's logo, a subtitle, and buttons for logging in, creating an account, and 
+resetting the password. 
+- The screen uses styles defined in the `styles` object to ensure a consistent 
+and visually appealing layout.
+- The `useRouter` hook from `expo-router` is used to navigate to the 
+appropriate screens when the buttons are pressed. 
+
+*/
 
 export default function LandingScreen() {
   const router = useRouter();
@@ -7,10 +19,14 @@ export default function LandingScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.title}>GMA Connect</Text>
+        <Image
+          source={require("../../../assets/images/gma-in-app-logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
         <Text style={styles.subtitle}>Connect - Grow - Thrive</Text>
       </View>
-
+      {/* Footer with navigation buttons */}
       <View style={styles.footer}>
         <TouchableOpacity
           style={styles.primaryButton}
@@ -23,11 +39,11 @@ export default function LandingScreen() {
           style={styles.secondaryButton}
           onPress={() => router.push("/register" as any)}
         >
-          <Text style={styles.secondaryButtonText}>Create Account</Text>
+          <Text style={styles.secondaryButtonText}>Register</Text>
         </TouchableOpacity>
 
-        <Link href={"/profile-setup" as any} asChild>
-          <Text style={styles.guestText}>Continue as guest</Text>
+        <Link href={"/forgot-password" as any} asChild>
+          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
         </Link>
       </View>
     </View>
@@ -37,60 +53,73 @@ export default function LandingScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#a64d79",
-    justifyContent: "space-between",
-    paddingVertical: 60,
+    backgroundColor: colors.background,
+    justifyContent: "center",
     paddingHorizontal: 24,
+    paddingTop: 120,
   },
+
   content: {
     flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 40,
   },
-  title: {
-    fontSize: 42,
-    fontWeight: "800",
-    color: "#ffffff",
-    marginBottom: 8,
-    textAlign: "center",
+
+  logo: {
+    width: 200,
+    height: 120,
+    padding: 0,
+    margin: 0,
   },
+
   subtitle: {
-    fontSize: 18,
-    color: "#f0f0f0",
+    fontSize: 16,
+    fontWeight: "700",
+    color: colors.saveBtnTextColor,
     textAlign: "center",
     opacity: 0.9,
+    marginTop: 16,
   },
+
   footer: {
     width: "100%",
-    gap: 16,
+    gap: 12,
+    alignItems: "center",
+    marginBottom: 220,
   },
+
   primaryButton: {
-    backgroundColor: "#25292e",
-    width: "100%",
+    backgroundColor: colors.primary,
+    width: "80%",
     paddingVertical: 16,
     borderRadius: 12,
     alignItems: "center",
   },
+
   primaryButtonText: {
-    color: "#ffffff",
+    color: colors.textOnPrimary,
     fontSize: 18,
     fontWeight: "700",
   },
+
   secondaryButton: {
-    backgroundColor: "#FAF0E4",
-    width: "100%",
+    backgroundColor: colors.textOnPrimary,
+    width: "80%",
     paddingVertical: 16,
     borderRadius: 12,
     alignItems: "center",
   },
+
   secondaryButtonText: {
-    color: "#25292e",
+    color: colors.primary,
     fontSize: 18,
     fontWeight: "700",
   },
-  guestText: {
+
+  forgotPasswordText: {
     marginTop: 8,
-    color: "#f0f0f0",
+    color: colors.saveBtnTextColor,
     fontSize: 15,
     textAlign: "center",
     textDecorationLine: "underline",
