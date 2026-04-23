0c3d0590266defcba654da984a306ac534fea026
tdoherty-alt <tdoherty@utas.edu.au>
2026-04-23 15:57:06 +1000

    Change:PHX233 changes to Payment Methods

--- src/screens/profile/profile-UI.tsx
+++ src/screens/profile/profile-UI.tsx
@@ -36,8 +36,8 @@ export default function ProfileUI({
     },
     {
       id: "payment",
-      label: "Manage payment methods",
-      path: "/(profile)/manage-payment-method", 
+      label: "Payment methods",
+      path: "/(profile)/payment-methods", 
     },
     
     {
